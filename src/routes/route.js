9f052016afaca23c5b9a6813b2a91a71dacbec06
jyoti987 <jyotisingh4748@gmail.com>
2022-08-08 16:13:03 +0530

    assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const lodash = require('lodash')
 const abc = require('../introduction/intro')
 const first = require('../logger/logger') //first problem
 const second = require('../util/helper') //second problem
@@ -13,6 +14,23 @@ router.get('/test-me', function (req, res) {
     first.welcome(); //1st problem
     second.printDate(); // 2nd problem
     third.thirdFunction(); // 3rd problem
+    let months = ['January', 'February','March','April','May','June','July','August','September','October','November','December']
+    let arr = lodash.chunk(months,3)
+    console.log(arr)
+
+    let arr2 = [3,5,7,9,11,13,15,17,19.21]
+    let result = lodash.tail(arr2,9)
+    console.log(result)
+
+    let arr3 =[1,1,2,2,3,3]
+    let result2 =lodash.union(arr3)
+    console.log(result2)
+
+    let arr4 = [["horror","The Shining"] ["drama","Titanic"] ["thriller","Shutter Island"] ["fantasy","Pans Labyrinth"]];
+    let result3 = {}
+    result3 = lodash.fromPairs(arr4,1)
+    console.log(result3)
+
 });
 
 
