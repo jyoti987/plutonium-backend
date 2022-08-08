3239c31e35a09eed0e9b39d477e6b83f7d7e27b9
jyoti987 <jyotisingh4748@gmail.com>
2022-08-08 12:59:01 +0530

    assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const abc = require('../introduction/intro')
+const first = require('../logger/logger') //first problem
+const second = require('../util/helper') //second problem
+const third = require('../validator/formatter')
 const router = express.Router();
+
 
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
     abc.printName()
     res.send('My second ever api!')
+    first.welcome(); //1st problem
+    second.printDate(); // 2nd problem
+    third.thirdFunction(); // 3rd problem
 });
 
 
