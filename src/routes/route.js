62883af28b3970734d9c24ee80554b404868a8e5
jyoti987 <jyotisingh4748@gmail.com>
2022-08-09 13:03:59 +0530

    assignment solution and debug

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,107 +1,124 @@
 const express = require('express');
-const myHelper = require('../util/helper')
-const underscore = require('underscore')
-
+const abc = require('../introduction/intro')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    myHelper.printDate()
-    myHelper.getCurrentMonth()
-    myHelper.getCohortData()
-    let firstElement = underscore.first(['Sabiha','Akash','Pritesh'])
-    console.log('The first element received from underscope function is '+firstElement)
-    res.send('My first ever api!')
+    console.log('My batch is', abc.name)
+    abc.printName()
+    logger.welcome()
+
+    res.send('My second ever api!')
 });
 
-router.get("/movies/:indexNumber", function(req, res){
-    const movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
-    console.log(req.params.indexNumber)
-    let movieIndex = req.params.indexNumber
-    //check index value. less than 0 or greater than array (length - 1) are not valid
-    if(movieIndex<0 || movieIndex>=movies.length) {
-        //if the index is invalid send an error message
-        return res.send('The index value is not correct, Please check the it')
+router.get('/students', function (req, res){
+    let students = ['Sabiha', 'Neha', 'Akash']
+    res.send(students)
+})
+
+router.get('/student-details/:name', function(req, res){
+    /*
+    params is an attribute inside request that contains 
+    dynamic values.
+    This value comes from the request url in the form of an 
+    object where key is the variable defined in code 
+    and value is what is sent in the request
+    */
+
+    let requestParams = req.params
+
+    // JSON strigify function helps to print an entire object
+    // We can use any ways to print an object in Javascript, JSON stringify is one of them
+    console.log("This is the request "+ JSON.stringify(requestParams))
+    let studentName = requestParams.name
+    console.log('Name of the student is ', studentName)
+    
+    res.send('Dummy response')
+})
+
+// --------------------- solution 1 -----------------------------------------
+
+
+router.get('/movie', function(req,res){
+    let moviesName = ['Rang de basanti', 'The shining' , 'Lord of the rings' , 'Batman begins'];
+    res.send(moviesName)
+    console.log(moviesName)
+
+})
+
+
+// ----------------------------solution 2 and 3 ---------------------------------
+
+
+router.get ('/movies/:indexNumber', (req,res) => {
+    const moviesName = ['Rang de basanti', 'The shining' , 'Lord of the rings' , 'Batman begins'];
+    let indexOfMovie = (req.params.indexNumber)
+    console.log( indexOfMovie)
+
+    if (indexOfMovie < 0 || indexOfMovie > moviesName.length){
+        res.send("please insert valid index")
     }
-
-    //if the index was valid send the movie at that index in response
-    let requiredMovie = movies[movieIndex]
-    res.send(requiredMovie)
+    else{
+        res.send(moviesName[indexOfMovie])
+    }
 })
 
-router.get("/shoes", function(req, res){
-    let queryParams = req.query
-    let brand = queryParams.brand
-    res.send("dummy response")
+// ---------------------------------solution 4 -----------------------------------------
+
+
+router.get ('/film', function(req,res){
+    const filmsObj = [ ['id: 1', "name: The Shining"], 
+    ['id: 2', "name: Incendies"], 
+    ['id: 3', "name: Rang de Basanti"], 
+    ['id: 4', "name: Finding Nemo"]]
+
+    res.send(filmsObj)
+
 })
 
-// uses query params
-router.get('/candidates', function(req, res){
-    console.log('Query paramters for this request are '+JSON.stringify(req.query))
-    let gender = req.query.gender
-    let state = req.query.state
-    let district = req.query.district
-    console.log('State is '+state)
-    console.log('Gender is '+gender)
-    console.log('District is '+district)
-    let candidates = ['Akash','Suman']
-    res.send(candidates)
-})
 
-// use path param
-router.get('/candidates/:canidatesName', function(req, res){
-    console.log('The request objects is '+ JSON.stringify(req.params))
-    console.log('Candidates name is '+req.params.canidatesName)
-    res.send('Done')
-})
 
-router.get("/films", function(req, res){
-    const films = [ {
-        "id": 1,
-        "name": "The Shining"
-       }, {
-        "id": 2,
-        "name": "Incendies"
-       }, {
-        "id": 3,
-        "name": "Rang de Basanti"
-       }, {
-        "id": 4,
-        "name": "Finding Nemo"
-       }]
-       //send all the films
-      res.send(films) 
-})
+// ---------------------------------------------Solution 5 ----------------------------------------------
 
-router.get("/films/:filmId", function(req, res){
-    const films = [ {
-        "id": 1,
-        "name": "The Shining"
-       }, {
-        "id": 2,
-        "name": "Incendies"
-       }, {
-        "id": 3,
-        "name": "Rang de Basanti"
-       }, {
-        "id": 4,
-        "name": "Finding Nemo"
-       }]
 
-       let filmId = req.params.filmId
+router.get ('/get-/films/:id', function(req,res){
+    let moviesObj = [ {"id": 1, "name": "The Shining"}, 
+    {"id": 2, "name": "Incendies"}, 
+    {"id": 3, "name": "Rang de Basanti"}, 
+    {"id": 4, "name": "Finding Nemo"}]
+    
+    var result = req.params.id
+    result1 = result - 1
+    if(result <= moviesObj.length){
+        res.send(moviesObj[result1])
+            
+    } else{
+        res.send("No movie exists with this id")
+    }
+    
+    })
 
-       //iterate all the films
-       //search for a film whose id matches with the id recevied in request
-       for(let i = 0; i < films.length; i++){
-           let film = films[i]
-           if(film.id == filmId) {
-               //if there is a match return the response from here
-               return res.send(film)
-           }
-       }
 
-       //if there is no match give an error response
-       res.send("The film id doesn't match any movie")
-})
+
+
+
+
+
+
+
+  
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
-// adding this comment for no reason
