71438e9ced98cef4ca61e6c7342bb8b2b00e86cd
jyoti987 <jyotisingh4748@gmail.com>
2022-08-12 23:13:36 +0530

    post API'S

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://jyoti-singh:hXv8ICQe0GBH9kpr@cluster0.ncvhe32.mongodb.net/?retryWrites=true&w=majority",{
+    useNewurlparser:true
+}) 
+.then( ()=> console.log("Mongodb is connected"))
+.catch(  err => console.log(err))
+
+
+
 app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function() {
--- src/routes/route.js
+++ src/routes/route.js
@@ -179,4 +179,57 @@ router.post("/players/:playerName/bookings/:bookingId", function (req, res) {
 
 
 
+//2nd assignment
+
+let persons= [
+  {
+  name: "SK",
+  age: 10,
+  votingStatus: false
+},
+{
+  name: "SK",
+  age: 20,
+  votingStatus: false
+},
+{
+  name: "AA",
+  age: 70,
+  votingStatus: false
+},
+{
+  name: "PK",
+  age: 5,
+  votingStatus: false
+},
+{
+  name: "HO",
+  age: 40,
+  votingStatus: false
+}
+]
+
+router.post('/voters',(req,res)=>{
+
+    let param = req.query;
+    let age = parseInt(param.age);
+    let voterArray = [];
+    
+
+   for(let i =0; i < persons.length ; i++){
+      if(persons[i].age >= age){
+           persons[i].votingStatus = true
+          voterArray.push(persons[i])  
+      }
+   }
+
+  res.send(voterArray)
+          
+})
+
+// module.exports = router;
+
+
+
+
 module.exports = router;
