15ce59491a899a265aa5cbff0f1ce202e0925205
jyoti987 <jyotisingh4748@gmail.com>
2022-08-16 17:48:42 +0530

    mongo class

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://jyoti-singh:hXv8ICQe0GBH9kpr@cluster0.ncvhe32.mongodb.net/Jyoti4748-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
