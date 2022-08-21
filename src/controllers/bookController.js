d6c4d1f725702e40af7a66c902268a9f3f1827ee
jyoti987 <jyotisingh4748@gmail.com>
2022-08-21 20:49:09 +0530

    refrence assignment

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,23 +1,97 @@
 const authorModel = require("../models/authorModel")
-const bookModel= require("../models/bookModel")
+ const publisherModel = require("../models/publisherModel")
+ const bookModel = require("../models/bookModel")
 
-const createBook= async function (req, res) {
-    let book = req.body
-    let bookCreated = await bookModel.create(book)
-    res.send({data: bookCreated})
-}
+ const createBook = async function (req, res) {
+     let book = req.body
+     let bookCreated = await bookModel.create(book)     
+     res.send({ data: bookCreated })
+ }
 
-const getBooksData= async function (req, res) {
-    let books = await bookModel.find()
-    res.send({data: books})
-}
+ 
 
-const getBooksWithAuthorDetails = async function (req, res) {
-    let specificBook = await bookModel.find().populate('author_id')
-    res.send({data: specificBook})
 
-}
+ const getBooksData = async function (req, res) {
+     let books = await bookModel.find()
+     res.send({ data: books })
+ }
 
-module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
-module.exports.getBooksWithAuthorDetails = getBooksWithAuthorDetails
+ const getBooksWithAuthorDetails = async function (req, res) {
+     let specificBook = await bookModel.find().populate(['author_id', 'publisher'])
+     res.send({ data: specificBook })
+
+ }
+
+
+ const getBookValid = async function (req, res) {
+     const book = req.body
+     const author = req.body.author_id
+     const publisher_id = req.body.publisher
+     const isValidAuthor = await authorModel.find({ _id: author }).select({ _id: 1 })
+     const isValidPublisher = await publisherModel.find({ _id: publisher_id }).select({ _id: 1 })
+
+
+     if (isValidAuthor.length > 0) {
+         if (isValidPublisher.length > 0) {
+             const newBook = await bookModel.create(book)
+             res.send({ newBook: newBook })
+         } else {
+             res.send({ msg: "Enter valid Publisher id" })
+         }
+     } else {
+         res.send({ msg: "Enter valid Author id" })
+     }
+
+
+
+ }
+
+
+
+
+
+ const bookUpdate = async function (req, res) {
+     const data = await publisherModel.find({ $or:  [{"name" : "Penguin"} , { "name" : "harperCollins" }] })
+     const updatedData=await bookModel.updateMany({publisher:data},{"hardCover":true})
+     res.send(updatedData);
+ }
+
+
+
+const priceUpdate = async function (req, res) {
+    const data = await authorModel.find({rating:{$gt:3.5}}).select({_id:1})
+    const update=await bookModel.updateMany({author_id:data},{$inc:{price:+10}},{new:true})
+
+    res.send(update);
+ }
+
+
+
+ module.exports.getBookValid = getBookValid
+ module.exports.createBook = createBook
+ module.exports.getBooksData = getBooksData
+ module.exports.getBooksWithAuthorDetails = getBooksWithAuthorDetails
+ module.exports.bookUpdate  = bookUpdate
+ module.exports.priceUpdate  = priceUpdate 
+// // module.exports.getBookValid = getBookValid
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const authorController= require("../controllers/authorController")
+const publisherController= require("../controllers/publisherController")
 const bookController= require("../controllers/bookController")
 
 router.get("/test-me", function (req, res) {
@@ -12,10 +13,20 @@ router.post("/createAuthor", authorController.createAuthor  )
 
 router.get("/getAuthorsData", authorController.getAuthorsData)
 
+router.post("/createPublisher", publisherController.createPublisher)
+
+router.get("/getPublishersData", publisherController.getPublisherData)
+
 router.post("/createBook", bookController.createBook  )
 
 router.get("/getBooksData", bookController.getBooksData)
 
 router.get("/getBooksWithAuthorDetails", bookController.getBooksWithAuthorDetails)
+
+router.post("/getBookValid", bookController.getBookValid)
+
+router.put("/bookUpdate", bookController.bookUpdate)
+
+router.put("/priceUpdate", bookController.priceUpdate)
 
 module.exports = router;
