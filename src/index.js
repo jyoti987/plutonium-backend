6203f4fc5697fa8022b0de124815478eac9832fd
jyoti987 <jyotisingh4748@gmail.com>
2022-08-25 12:04:59 +0530

    assignment middleware

--- src/index.js
+++ src/index.js
@@ -11,15 +11,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
 
-app.use (
+app.use(
     function (req, res, next) {
-        console.log ("inside GLOBAL MW");
+        console.log("inside GLOBAL MW");
+        const moment = require('moment');
+        const today = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+        let ipAddress = req.ip;
+        let url = req.originalUrl
+        console.log (today +" , "+ipAddress+" , "+url);
         next();
-  }
-  );
+    }
+);
 
 app.use('/', route);
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -11,6 +11,12 @@ router.get("/test-me", function (req, res) {
 
 
 
+router.get("/assignment", function (req, res) {
+    res.send("Today assignment is done")
+})
+
+
+
 
 router.post("/createBook", BookController.createBook  )
 
