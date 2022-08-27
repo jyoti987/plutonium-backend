4fd625470019afefe89241a8547ae7e54617d4e4
jyoti987 <jyotisingh4748@gmail.com>
2022-08-28 02:55:12 +0530

    middleware2 assignment

--- src/middlewares/commonMiddlewares.js
+++ src/middlewares/commonMiddlewares.js
@@ -1,24 +1,24 @@
 
-const mid1= function ( req, res, next) {
-    req.falana= "hi there. i am adding something new to the req object"
-    console.log("Hi I am a middleware named Mid1")
-    next()
-}
+// const mid1= function ( req, res, next) {
+//     req.falana= "hi there. i am adding something new to the req object"
+//     console.log("Hi I am a middleware named Mid1")
+//     next()
+// }
 
-const mid2= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid2")
-    next()
-}
+// const mid2= function ( req, res, next) {
+//     console.log("Hi I am a middleware named Mid2")
+//     next()
+// }
 
-const mid3= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid3")
-    next()
-}
+// const mid3= function ( req, res, next) {
+//     console.log("Hi I am a middleware named Mid3")
+//     next()
+// }
 
-const mid4= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid4")
-    next()
-}
+// const mid4= function ( req, res, next) {
+//     console.log("Hi I am a middleware named Mid4")
+//     next()
+// }
 
 
 
@@ -27,7 +27,6 @@ const mid4= function ( req, res, next) {
 
 const validation = function(req,res,next){
     let toekenHeader = req.headers.isfreeappuser
-    console.log(toekenHeader)
     if(!toekenHeader){
         res.send("please enter isFreeAppUser with any value")
     } else{
@@ -41,8 +40,8 @@ const validation = function(req,res,next){
 
 
 
-module.exports.mid1= mid1
-module.exports.mid2= mid2
-module.exports.mid3= mid3
-module.exports.mid4= mid4
+// module.exports.mid1= mid1
+// module.exports.mid2= mid2
+// module.exports.mid3= mid3
+// module.exports.mid4= mid4
 module.exports.validation = validation
