1877035145c6862537a067de8e0011e535983cb4
jyoti987 <jyotisingh4748@gmail.com>
2022-09-02 18:11:44 +0530

    axios assignment

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -78,8 +78,85 @@ let getOtp = async function (req, res) {
     }
 }
 
+// ====================================Assignment problem 1 ===================================================
+
+let getByDistrictId = async function (req, res) {
+    try {
+        let id = req.query.district_id
+        let date = req.query.date
+        console.log(`query params are: ${id} ${date}`)
+        var options = {
+            method: "get",
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${id}&date=${date}`
+        }
+        let result = await axios(options)
+        console.log(result.data)
+        res.status(200).send({ msg: result.data })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+
+// ============================ problem 2 ==================================================
+
+
+let getWeatherByCities = async function (req, res) {
+    let sortedCity = []
+    try {
+        let city = ["Bengaluru", "Mumbai", "Delhi", "Kolkata", "Chennai", "London", "Moscow"]
+        for (let i = 0; i < city.length; i++) {
+            q = city[i]
+            var options = {
+                method: "get",
+                url: `http://api.openweathermap.org/data/2.5/weather?q=${q}&appid=1362dae990936264cdd25b4ceb4d9f18`
+            }
+            let result = await axios(options)
+
+            sortedCity.push({ city: city[i], temp: result.data.main.temp, })
+        }
+        res.status(200).send({ data: sortedCity.sort((a, b) => a.temp - b.temp) })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+
+
+
+// ===================================================problem 3 ==============================================================
+
+let memeQuestion = async function (req, res){
+    try {
+        let options = {
+            method: "post",
+            url: "https://api.imgflip.com/caption_image?template_id=4087833&text0=after solve an error&username=chewie12345&password=meme@123"
+        }
+        let result = await axios(options)
+        res.send({data: result.data})
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+
+
+
+
+
+
+
+
 
 module.exports.getStates = getStates
 module.exports.getDistricts = getDistricts
 module.exports.getByPin = getByPin
 module.exports.getOtp = getOtp
+module.exports.getByDistrictId = getByDistrictId
+module.exports.memeQuestion = memeQuestion
+module.exports.getWeatherByCities = getWeatherByCities
--- src/routes/route.js
+++ src/routes/route.js
@@ -15,6 +15,12 @@ router.get("/cowin/getByPin", CowinController.getByPin)
 
 router.post("/cowin/getOtp", CowinController.getOtp)
 
+router.get("/cowin/getByDistrictId", CowinController.getByDistrictId)
+
+router.get("/getWeatherByCities", CowinController.getWeatherByCities)
+
+router.post("/memes", CowinController.memeQuestion)
+
 // WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
 
 
